211bbdb96aeea46ca5d22c3302c56974f3fcbdab
mignot <andreas.garcia@bodzify.com>
2024-06-01 14:18:32 +0200

    Print tested fingerprint for analysis

--- audio_fingerprint_generator/audio_fingerprint_generator.py
+++ audio_fingerprint_generator/audio_fingerprint_generator.py
@@ -36,7 +36,9 @@ def get_fingerprint_and_duration_from_file_path(file_path: str) -> Tuple[Optiona
             raise error
 
     try:
-        return acoustid.fingerprint_file(path=file_path)
+        duration, fingerprint = acoustid.fingerprint_file(path=file_path)
+        print(file_path)
+        print(f'Fingerprint: {fingerprint}')
     except acoustid.FingerprintGenerationError as error:
         if error.args[0] == 'fpcalc exited with status 2':
             raise AudioFileProbablyTooShortForFingerprintGenerationError(
