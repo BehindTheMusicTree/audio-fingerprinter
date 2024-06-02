cac2203a5a8f1920e4810271e222ad025b4ad55d
mignot <andreas.garcia@bodzify.com>
2024-06-02 22:32:13 +0200

    Convert env PORT variable to int

--- config.py
+++ config.py
@@ -5,31 +5,37 @@ import dotenv
 
 dotenv.load_dotenv()
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
 
+
 class DevConfig(Config):
     DEBUG = True
+
 
 class GithubCiTestConfig(Config):
     TESTING = True
     DEBUG = False
 
+
 class TestConfig(Config):
     TESTING = True
     DEBUG = True
 
+
 class ProdConfig(Config):
     TESTING = False
     DEBUG = False
-    
+
+
 class ENV_VALUES:
     DEV = 'DEV'
     GITHUB_CI_TEST = 'GITHUB_CI'
     TEST = 'TEST'
     PROD = 'PROD'
-    
+
 
 ENV = os.getenv('ENV')
-PORT = os.getenv('PORT')
+PORT = int(os.getenv('PORT'))  # type: ignore
