8bff8683fa3a7ba9442b93e49bd4275728463615
mignot <andreas.garcia@bodzify.com>
2024-06-02 14:27:52 +0200

    Add env configurations

--- config.py
+++ config.py
@@ -5,10 +5,30 @@ import dotenv
 
 dotenv.load_dotenv()
 
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+class DevConfig(Config):
+    DEBUG = True
+
+class GithubCiTestConfig(Config):
+    TESTING = True
+    DEBUG = False
+
+class TestConfig(Config):
+    TESTING = True
+    DEBUG = True
+
+class ProdConfig(Config):
+    TESTING = False
+    DEBUG = False
+    
 class ENV_VALUES:
     DEV = 'DEV'
-    GITHUB_CI = 'GITHUB_CI'
+    GITHUB_CI_TEST = 'GITHUB_CI'
     TEST = 'TEST'
+    PROD = 'PROD'
     
 
 ENV = os.getenv('ENV')
