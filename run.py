6dfedb191475139de8e2dddfcd9ef5b83959d9f7
mignot <andreas.garcia@bodzify.com>
2024-06-02 16:32:12 +0200

    Set api fields as constants

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ import base64
 from flask import Flask, request
 
 import config as config
-from app.audio_fingerprint_generator import FpcalcStatus2Error, WrongFileExtensionError, WrongFileTypeError, get_fingerprint_and_duration_from_file
+from app.audio_fingerprint_generator \
+    import FpcalcStatus2Error, WrongFileExtensionError, WrongFileTypeError, FileNotFoundError, \
+    get_fingerprint_and_duration_from_file
+
 
 def create_app():
     app = Flask(__name__)
@@ -22,24 +25,34 @@ def create_app():
     def error_response(message, status):
         return {'status': status, 'message': message}, status
 
+    class POST_FIELDS:
+        FILE_PATH: str = 'filepath'
+
+    class RESPONSE_FIELDS:
+        DURATION: str = 'duration'
+        FINGERPRINT: str = 'fingerprint'
+
     @app.route('/generate-audio-fingerprint', methods=['POST'])
     def generate_audio_fingerprint():
-        if 'filepath' not in request.json:
+        if POST_FIELDS.FILE_PATH not in request.json:
             return error_response('No filepath in the request', 400)
-        filepath = request.json['filepath']
+        filepath = request.json[POST_FIELDS.FILE_PATH]
         try:
             duration, fingerprint = get_fingerprint_and_duration_from_file(filepath)
             fingerprint_b64 = base64.b64encode(fingerprint).decode()
-            return {'duration': duration, 'fingerprint': fingerprint_b64}
+            return {RESPONSE_FIELDS.DURATION: duration, RESPONSE_FIELDS.FINGERPRINT: fingerprint_b64}
         except Exception as e:
             error_message = str(e)
-            if isinstance(e, FileNotFoundError) or isinstance(e, WrongFileExtensionError) or isinstance(e, WrongFileTypeError):
+            if (isinstance(e, FileNotFoundError) or
+                    isinstance(e, WrongFileExtensionError) or
+                    isinstance(e, WrongFileTypeError)):
                 return error_response(error_message, 400)
             if isinstance(e, FpcalcStatus2Error):
                 return error_response(error_message, 500)
             return error_response(error_message, 500)
 
     return app
+
 
 app = create_app()
 
