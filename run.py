2fd013b9eb26a5c5031f9d5bb8f11482df3dde43
mignot <andreas.garcia@bodzify.com>
2024-06-02 14:28:33 +0200

    Add create app function

---
+++ run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import base64
+from flask import Flask, request
+
+import config as config
+from app.audio_fingerprint_generator import FpcalcStatus2Error, WrongFileExtensionError, WrongFileTypeError, get_fingerprint_and_duration_from_file
+
+def create_app():
+    app = Flask(__name__)
+    if config.ENV == config.ENV_VALUES.TEST:
+        app.config.from_object(config.TestConfig)
+    elif config.ENV == config.ENV_VALUES.DEV:
+        app.config.from_object(config.DevConfig)
+    elif config.ENV == config.ENV_VALUES.GITHUB_CI_TEST:
+        app.config.from_object(config.GithubCiTestConfig)
+    elif config.ENV == config.ENV_VALUES.PROD:
+        app.config.from_object(config.ProdConfig)
+    else:
+        raise ValueError(f'Invalid ENV value {config.ENV}')
+
+    def error_response(message, status):
+        return {'status': status, 'message': message}, status
+
+    @app.route('/generate-audio-fingerprint', methods=['POST'])
+    def generate_audio_fingerprint():
+        if 'filepath' not in request.json:
+            return error_response('No filepath in the request', 400)
+        filepath = request.json['filepath']
+        try:
+            duration, fingerprint = get_fingerprint_and_duration_from_file(filepath)
+            fingerprint_b64 = base64.b64encode(fingerprint).decode()
+            return {'duration': duration, 'fingerprint': fingerprint_b64}
+        except Exception as e:
+            error_message = str(e)
+            if isinstance(e, FileNotFoundError) or isinstance(e, WrongFileExtensionError) or isinstance(e, WrongFileTypeError):
+                return error_response(error_message, 400)
+            if isinstance(e, FpcalcStatus2Error):
+                return error_response(error_message, 500)
+            return error_response(error_message, 500)
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=config.PORT)
