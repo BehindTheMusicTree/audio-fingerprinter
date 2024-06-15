56b9c843557b281551343689b58a5e4171295a44
mignot <andreas.garcia@bodzify.com>
2024-06-15 06:34:38 +0200

    Fix CI tests

--- scripts/setup_filesystem.github_ci_test.sh
+++ scripts/setup_filesystem.github_ci_test.sh
@@ -4,3 +4,8 @@ poolAbsPath=/tmp/bodzify-audio-fingerprinter/pool/
 sudo mkdir -p $poolAbsPath
 sudo chmod 775 $poolAbsPath
 sudo chown -R $USER $poolAbsPath
+
+logRelPath=log/
+sudo mkdir -p $logRelPath
+sudo chmod 775 $logRelPath
+sudo chown -R $USER $logRelPath
