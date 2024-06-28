7b299a306440a18ec9106129a660756db9ad7b4b
mignot <andreas.garcia@bodzify.com>
2024-06-28 13:39:25 +0200

    Improve script var naming

--- scripts/setup_filesystem.sh
+++ scripts/setup_filesystem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/
-echo "SCRIPT_DIR is set to $SCRIPT_DIR"
+SCRIPTS_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/
+echo "SCRIPTS_DIR is set to $SCRIPTS_DIR"
 source "${SCRIPTS_DIR}/env_config/set_env_from_config.sh"
 
 if [ ! -d "$POOL_DIR" ]; then
