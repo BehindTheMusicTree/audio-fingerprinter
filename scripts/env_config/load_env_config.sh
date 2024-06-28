7b5fb315cc017ac6b1aa4798fac7b845af0a1a59
mignot <andreas.garcia@bodzify.com>
2024-06-28 13:54:26 +0200

    Fix env loading

--- scripts/env_config/load_env_config.sh
+++ scripts/env_config/load_env_config.sh
@@ -12,32 +12,31 @@ if [ -f "$ENV_PATH" ]; then
         if [ -z "$key" ]; then continue; fi
         export "$key=$value"
     done < "$ENV_PATH"
-
-	if [ -z $ENV ]; then
-		echo "ENV is not set"
-		exit 1
-    fi
-
-    CONFIG_PATH="$SCRIPT_DIR/../../config/env_config.json"
-    if [ ! -f "$CONFIG_PATH" ]; then
-        echo "Configuration file '${CONFIG_PATH}' was not found."
-        exit 1
-    else
-		echo "Loading configuration values from ${CONFIG_PATH}..."
-	fi
-
-    config=$(cat "$CONFIG_PATH")
-    currentEnvLower=$(echo "$ENV" | tr '[:upper:]' '[:lower:]')
-
-    envConfig=$(echo "$config" | jq -r --arg env "$currentEnvLower" '.["environments"][$env]')
-
-	export EXTERNAL_DIRS_NEEDED=$(echo "$envConfig" | jq -r '.externalDirsNeeded')
-
-	defaultInternalPaths=$(echo "$config" | jq -r '.["defaultInternalPaths"]')
-
-    export POOL_DEFAULT_INTERNAL_DIR=$(echo "$defaultInternalPaths" | jq -r '.["pool"]')
-    export LOG_DEFAULT_INTERNAL_DIR=$(echo "$defaultInternalPaths" | jq -r '.["log"]')
-
 else
     echo "$ENV_PATH file does not exist"
 fi
+
+if [ -z $ENV ]; then
+    echo "ENV is not set"
+    exit 1
+fi
+
+CONFIG_PATH="$SCRIPT_DIR/../../config/env_config.json"
+if [ ! -f "$CONFIG_PATH" ]; then
+    echo "Configuration file '${CONFIG_PATH}' was not found."
+    exit 1
+else
+    echo "Loading configuration values from ${CONFIG_PATH}..."
+fi
+
+config=$(cat "$CONFIG_PATH")
+currentEnvLower=$(echo "$ENV" | tr '[:upper:]' '[:lower:]')
+
+envConfig=$(echo "$config" | jq -r --arg env "$currentEnvLower" '.["environments"][$env]')
+
+export EXTERNAL_DIRS_NEEDED=$(echo "$envConfig" | jq -r '.externalDirsNeeded')
+
+defaultInternalPaths=$(echo "$config" | jq -r '.["defaultInternalPaths"]')
+
+export POOL_DEFAULT_INTERNAL_DIR=$(echo "$defaultInternalPaths" | jq -r '.["pool"]')
+export LOG_DEFAULT_INTERNAL_DIR=$(echo "$defaultInternalPaths" | jq -r '.["log"]')
