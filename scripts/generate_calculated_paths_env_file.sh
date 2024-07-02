8096596427b83587c40a442281babac2e5f6303e
mignot <andreas.garcia@bodzify.com>
2024-07-02 20:46:54 +0200

    Fix filesystem setup

--- scripts/generate_calculated_paths_env_file.sh
+++ scripts/generate_calculated_paths_env_file.sh
@@ -13,34 +13,6 @@ if [ -z "$2" ]; then
 fi
 GENERATED_PATHS_ENV_FILE=$2
 
-required_vars=("APP_IS_EXPOSED")
-if [ -z "$APP_IS_EXPOSED" ]; then
-    echo "APP_IS_EXPOSED must be set" >&2
-    exit 1
-fi
-APP_IS_EXPOSED=$(echo "$APP_IS_EXPOSED" | tr '[:upper:]' '[:lower:]')
-
-
-if [ $APP_IS_EXPOSED != "true" ] && [ $APP_IS_EXPOSED != "false" ]; then
-    echo "APP_IS_EXPOSED must be set to true or false" >&2
-    exit 1
-fi
-
-if [ $APP_IS_EXPOSED = "true" ]; then
-    echo "APP_IS_EXPOSED is set to true"
-    if [ -z "$FLASK_LOG_DIR" ]; then
-        echo "FLASK_LOG_DIR must be set" >&2
-        exit 1
-    fi
-else
-    echo "APP_IS_EXPOSED is set to false"
-    if [ -z "$FLASK_LOG_INTERNAL_DIR" ]; then
-        echo "FLASK_LOG_INTERNAL_DIR must be set" >&2
-        exit 1
-    fi
-    FLASK_LOG_DIR=${BASE_DIR}${FLASK_LOG_INTERNAL_DIR}
-fi
-
 required_vars=("FLASK_LOGS_ARE_NEEDED")
 if [ -z "$FLASK_LOGS_ARE_NEEDED" ]; then
     echo "FLASK_LOGS_ARE_NEEDED must be set" >&2
@@ -54,11 +26,37 @@ if [ $FLASK_LOGS_ARE_NEEDED != "true" ] && [ $FLASK_LOGS_ARE_NEEDED != "false" ]
 fi
 
 if [ $FLASK_LOGS_ARE_NEEDED = "true"]; then
+    if [ -z "$APP_IS_EXPOSED" ]; then
+        echo "APP_IS_EXPOSED must be set" >&2
+        exit 1
+    fi
+    APP_IS_EXPOSED=$(echo "$APP_IS_EXPOSED" | tr '[:upper:]' '[:lower:]')
+
+    if [ $APP_IS_EXPOSED != "true" ] && [ $APP_IS_EXPOSED != "false" ]; then
+        echo "APP_IS_EXPOSED must be set to true or false" >&2
+        exit 1
+    fi
+
+    if [ $APP_IS_EXPOSED = "true" ]; then
+        echo "APP_IS_EXPOSED is set to true"
+        if [ -z "$FLASK_LOG_DIR" ]; then
+            echo "FLASK_LOG_DIR must be set" >&2
+            exit 1
+        fi
+    else
+        echo "APP_IS_EXPOSED is set to false"
+        if [ -z "$FLASK_LOG_INTERNAL_DIR" ]; then
+            echo "FLASK_LOG_INTERNAL_DIR must be set" >&2
+            exit 1
+        fi
+        FLASK_LOG_DIR=${BASE_DIR}${FLASK_LOG_INTERNAL_DIR}
+    fi
+
+    if [ -f $GENERATED_PATHS_ENV_FILE ]; then
+        rm -f $GENERATED_PATHS_ENV_FILE
+    fi
+    touch $GENERATED_PATHS_ENV_FILE
+
     echo "FLASK_LOG_DIR: $FLASK_LOG_DIR"
     echo "FLASK_LOG_DIR=$FLASK_LOG_DIR" >> "$GENERATED_PATHS_ENV_FILE"
 fi
-
-if [ -f $GENERATED_PATHS_ENV_FILE ]; then
-    rm -f $GENERATED_PATHS_ENV_FILE
-fi
-touch $GENERATED_PATHS_ENV_FILE
