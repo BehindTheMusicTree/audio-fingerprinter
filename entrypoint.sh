f660c8af6364860a291e1366a964cb8ae40dd71b
mignot <andreas.garcia@bodzify.com>
2024-09-09 15:08:12 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -z "$AUDIO_FINGERPRINTER_PORT" ]; then
-    echo "AUDIO_FINGERPRINTER_PORT is not set" >&2
+if [ -z "$APP_PORT" ]; then
+    echo "APP_PORT is not set" >&2
     exit 1
 fi
 
 exec gunicorn bodzify_api.wsgi:application \
-    --bind 0.0.0.0:${AUDIO_FINGERPRINTER_PORT} \
+    --bind 0.0.0.0:${APP_PORT} \
     --error-logfile=${GUNICORN_LOG_DIR_SYMLINK_TARGET}${GUNICORN_LOG_ERROR_FILENAME} \
     --access-logfile=${GUNICORN_LOG_DIR_SYMLINK_TARGET}${GUNICORN_LOG_ACCESS_FILENAME} \
     --log-level=info
