5d2f85f12f804ee8751f371ea999e56fc84c504c
mignot <andreas.garcia@bodzify.com>
2024-09-09 22:09:26 +0200

    Add logs to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+echo "Starting entrypoint script"
+
 if [ -z "$APP_PORT" ]; then
     echo "APP_PORT is not set" >&2
     exit 1
 fi
+
+echo "APP_PORT: $APP_PORT"
+echo "GUNICORN_LOG_DIR: $GUNICORN_LOG_DIR"
+echo "GUNICORN_LOG_ERROR_FILENAME: $GUNICORN_LOG_ERROR_FILENAME"
+echo "GUNICORN_LOG_ACCESS_FILENAME: $GUNICORN_LOG_ACCESS_FILENAME"
+
 
 exec gunicorn bodzify_api.wsgi:application \
     --bind 0.0.0.0:${APP_PORT} \
