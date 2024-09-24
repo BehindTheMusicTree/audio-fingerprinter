aa0e1edaaa0b411528cea0a2d373572eddd7e843
mignot <andreas.garcia@bodzify.com>
2024-09-24 17:17:23 +0200

    Fix script name typo

--- scripts/setup-filesystem.sh
+++ scripts/setup-filesystem.sh
@@ -30,7 +30,7 @@ load_app_calculated_paths_env_vars() {
     fi
 
     local CALCULATED_PATHS_ENV_FILE="${CALTULATED_PATHS_DIR}.env"
-    bash "${SCRIPTS_DIR}generate_calculated_paths_env_file.sh"
+    bash "${SCRIPTS_DIR}generate-calculated-paths-env-file.sh"
     if [ $? -ne 0 ]; then
         echo "Failed to generate calculated paths env file: $output" >&2
         exit 1
